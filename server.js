860cb13fe872c20d59cded49aa95c325ade71085
jyip01 <64282396+jyip01@users.noreply.github.com>
2020-09-08 12:10:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 // your code here!
-
+app.get("/echo/:something", (req, res) => {
+  res.json({
+    hostname: req.hostname,
+    query: req.query,
+    params: req.params
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
